e58cc670d64274110e9e3d81a1dfa5531bbc9d4e
NageswaraRaoMaridu <maridu.nageswararao@gmail.com>
2020-05-11 17:41:28 +0530

    Added maven copy sources plugin to include crnk-gen plugin generated sources

--- src/main/java/com/practice/crnk/resources/UniversityResource.java
+++ src/main/java/com/practice/crnk/resources/UniversityResource.java
@@ -8,6 +8,7 @@ import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.annotations.JsonApiRelation;
 import io.crnk.core.resource.annotations.JsonApiResource;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import static com.practice.crnk.resources.UniversityResource.RESOURCE_PATH;
 import static com.practice.crnk.resources.UniversityResource.RESOURCE_TYPE;
@@ -17,6 +18,7 @@ import static com.practice.crnk.resources.UniversityResource.RESOURCE_TYPE;
  * @since May 08, 2020
  */
 @Data
+@NoArgsConstructor
 @JsonApiResource(type = RESOURCE_TYPE, resourcePath = RESOURCE_PATH, sortable = true, filterable = true)
 public class UniversityResource {
 
